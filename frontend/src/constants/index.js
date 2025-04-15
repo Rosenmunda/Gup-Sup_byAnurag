17b8d3ddec4529a1a3a902f23766991215e9aacc
Anurag Sen <118921364+Rosenmunda@users.noreply.github.com>
2025-04-15 16:55:18 +0530

    project done

---
+++ frontend/src/constants/index.js
@@ -0,0 +1,34 @@
+export const THEMES = [
+  "light",
+  "dark",
+  "cupcake",
+  "bumblebee", // was "burnblebee"
+  "emerald",
+  "corporate",
+  "synthwave",
+  "retro",
+  "cyberpunk",
+  "valentine",
+  "halloween",
+  "garden",
+  "forest",
+  "aqua",
+  "lofi", // was "loft"
+  "pastel",
+  "fantasy",
+  "wireframe",
+  "black",
+  "luxury",
+  "dracula",
+  "cmyk", // was "crayk"
+  "autumn",
+  "business",
+  "acid",
+  "lemonade", // was "lamorade"
+  "night",
+  "coffee",
+  "winter",
+  "dim",
+  "nord",
+  "sunset"
+];
